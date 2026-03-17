c1c665e31f793298133e030b77fdb2968da2fb96
DinhNam1611 <luongdinhnam123abc@gmail.com>
2026-03-17 10:20:41 +0700

    Lab2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { About } from './about/about';
 import { Stories } from './pages/stories/stories';
+import { Contact } from './pages/contact/contact';
+import { Products } from './pages/products/products';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'about', component: About },
+  { path: 'contact', component: Contact },
+  { path: 'product', component: Products },
   { path: 'stories', component: Stories },
 ];
